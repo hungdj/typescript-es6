e742f2df9bfb5730b308ecc0f36dbbe09cba8ad8
hungdj <hungdx@outlook.com>
2020-04-07 15:28:56 +0700

    add new

---
+++ dist/lesson15.js
@@ -0,0 +1,5 @@
+"use strict";
+var name = 'TEDU';
+var message = 'Welcome ${name} to ES6 and Typescript';
+console.log(message);
+//# sourceMappingURL=lesson15.js.map
---
+++ lesson15.ts
@@ -0,0 +1,4 @@
+var name = 'TEDU';
+var message = 'Welcome ${name} to ES6 and Typescript';
+
+console.log(message);
