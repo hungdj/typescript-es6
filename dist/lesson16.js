7f6286620bcc2eb7e9ab068a4c9d7bb08564bd2f
hungdj <hungdx@outlook.com>
2020-04-07 15:43:36 +0700

    add lesson16

---
+++ dist/lesson16.js
@@ -0,0 +1,6 @@
+"use strict";
+var colorNames = ['Red', 'Green', 'Blue', 'Yellow', 'Pink'];
+for (var index in colorNames) {
+    console.log(colorNames[index]);
+}
+//# sourceMappingURL=lesson16.js.map
---
+++ lesson16.ts
@@ -0,0 +1,5 @@
+var colorNames = ['Red', 'Green', 'Blue', 'Yellow', 'Pink'];
+
+for(let index in colorNames){
+    console.log(colorNames[index]);
+}
